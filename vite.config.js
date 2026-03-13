d081fb87f8304c69720ca9d91b3240edd4d43fe9
novalove <juhyun.lee@jennifersoft.com>
2026-03-13 10:53:25 +0900

    iframe을 위한 보안정책 관련 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,22 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5177,
-        strictPort: false,
+        strictPort: true,
         proxy: {
             '/api': {
                 target: 'https://13.113.18.12:8443',
                 changeOrigin: true,
-                secure: false // 스스로 서명한 인증서 에러 우회
+                secure: false, // 스스로 서명한 인증서 에러 우회
+                onProxyRes: (proxyRes) => {
+                    proxyRes.headers['Cross-Origin-Resource-Policy'] = 'cross-origin';
+                }
             }
+        },
+        headers: {
+            'Content-Security-Policy': "frame-ancestors 'self' * file://*",
+            'Cross-Origin-Resource-Policy': 'cross-origin',
+            'Cross-Origin-Embedder-Policy': 'credentialless',
+            'Cross-Origin-Opener-Policy': 'same-origin'
         }
     }
 });
