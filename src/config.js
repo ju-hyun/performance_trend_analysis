d6c9fc92562513606865ff01265578d5d1a9c02c
novalove <juhyun.lee@jennifersoft.com>
2026-03-16 18:36:40 +0900

    config.js수정

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ export const config = {
   BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:7900/',
 
   // API 서버의 도메인 또는 IP (프록시를 사용하지 않고 직접 호출할 경우 적용)
-  API_DOMAIN: import.meta.env.VITE_API_EXTERNAL_URL || 'https://13.113.18.12:8443',
+  API_DOMAIN: import.meta.env.VITE_API_EXTERNAL_URL || '',
 
   // API 인증에 사용될 토큰 (.env 파일의 VITE_API_TOKEN 참조)
-  TOKEN: import.meta.env.VITE_API_TOKEN || 'd5HJuONXNZf'
+  TOKEN: import.meta.env.VITE_API_TOKEN || ''
 };
